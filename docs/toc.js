d082832bd7ef43795fc6423d0dd8d72595bced9f
bingfam <bingfam@outlook.kr>
2024-12-01 14:01:45 +0900

    update

--- docs/toc.js
+++ docs/toc.js
@@ -8,7 +8,7 @@ class MDBookSidebarScrollbox extends HTMLElement {
         super();
     }
     connectedCallback() {
-        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><a href="chapter_1.html"><strong aria-hidden="true">1.</strong> Chapter 1</a></li></ol>';
+        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><a href="utility/utility.html"><strong aria-hidden="true">1.</strong> 유틸리티</a></li><li><ol class="section"><li class="chapter-item expanded "><a href="utility/joplin.html"><strong aria-hidden="true">1.1.</strong> joplin</a></li></ol></li></ol>';
         // Set the current, active page, and reveal it if it's hidden
         let current_page = document.location.href.toString();
         if (current_page.endsWith("/")) {
